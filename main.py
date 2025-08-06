3036684d37b7fe115450f9a298646ff1fbf7d134
Moazzam Shaikh <moazzamshaikh8982@gmail.com>
2025-08-06 15:08:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-# main.py (Final Simple Version)
-import os
-import logging
 from app import create_app
 
-# Gunicorn is file ko load karega aur 'app' variable ko dhoondhega
+# Gunicorn is 'app' variable ko dhoondhega
 app = create_app()
 
-# Yeh hissa sirf local machine par test karne ke liye hai
+# Local testing ke liye
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    logging.info(f"🚀 Starting Bot locally on port {port}...")
-    # debug=True local testing ke liye aasan hai
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
